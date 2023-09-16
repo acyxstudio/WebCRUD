2b286facd5f4d0e16f461bd8544aaa6c57aee71f
Carlos Hopfer <developer@acyxstudio.com>
2023-09-16 14:48:48 +0100

    PUT Implemented

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
+using System.Linq.Expressions;
 using WebCRUD.Data;
 
 namespace WebCRUD.Controllers
@@ -36,13 +37,38 @@ namespace WebCRUD.Controllers
         public async Task<ActionResult<List<Person>>> GetPerson(int id)
         {
             var _personFound = await _context.PersonsTable.FindAsync(id);
+
             if (_personFound == null)
             {
                 return BadRequest("Person not found!");
             }
             return Ok(_personFound);
         }
-        
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Person>>> UpdatePerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest("Id and Person does not match!");
+            }
+            
+            
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+              await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {    
+               return NotFound();
+            }
+
+            return NoContent();
+            
+        }
+
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<Person>>> DeletePerson(int id)
         {
